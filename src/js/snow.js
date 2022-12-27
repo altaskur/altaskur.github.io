376401a804cbdb00ae19ec33d1b6c6f0978cb2b2
altaskur <altaskur@gmai.com>
2022-12-27 18:41:42 +0100

    cange snowflake rain

--- src/js/snow.js
+++ src/js/snow.js
@@ -3,26 +3,34 @@ export function generateSnowflake () {
   const snowflakeContainer = document.getElementById('snowflake-container')
 
   snowflake.setAttribute('viewBox', '0 0 100 100')
-  snowflake.setAttribute('width', '20')
-  snowflake.setAttribute('height', '20')
+  const w = Math.random() * 130
+
+  snowflake.setAttribute('width', w)
+  snowflake.setAttribute('height', w)
   snowflake.setAttribute('class', 'snowflake')
 
-  // Calcula una posición aleatoria en el eje x y la establece usando "style left"
   const x = Math.random() * window.innerWidth
   snowflake.setAttribute('style', `left: ${x}px;`)
 
   // Elimina el último copo de nieve generado
-  if (snowflakeContainer.childElementCount > 10) {
+  if (snowflakeContainer.childElementCount > 100) {
     snowflakeContainer.firstChild.remove()
   }
 
   const group = document.createElementNS('http://www.w3.org/2000/svg', 'g')
   group.innerHTML = `
-        <polygon points="50,0 100,50 50,100 0,50" fill="#FFFFFF" />
-        <circle cx="50" cy="50" r="40" fill="#FFFFFF" />
-        <circle cx="90" cy="20" r="30" fill="#FFFFFF" />
-        <circle cx="10" cy="80" r="25" fill="#FFFFFF" />
-        <path d="M50,50 L90,20 L10,80 Z" stroke="#FFFFFF" stroke-width="2" fill="none" />
+  <circle
+  style="fill:#b3b3b3;stroke-width:0.281721"
+  id="path111"
+  cx="5.7523947"
+  cy="5.2550101"
+  r="4.4169788" />
+<circle
+  style="fill:#ffffff;stroke-width:0.225728"
+  id="path113"
+  cx="5.4782648"
+  cy="5.0349364"
+  r="3.8632853" />
       `
 
   snowflake.appendChild(group)
