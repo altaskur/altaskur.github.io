be3ecdd665f4eb874e835b793a870dde35fcb9b3
altaskur <105789412+altaskur@users.noreply.github.com>
2025-02-16 23:21:42 +0100

    feat: :sparkles: add wave component and update icon imports; improve CSS variables

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as LinkedIn } from "./LinkedIn.astro";
+export { default as TickTock } from "./TickTock.astro";
+export { default as YouTubeIcon } from "./YouTubeIcon.astro";
+export { default as CodePen } from "./CodePen.astro";
+export { default as GitHub } from "./GitHub.astro";
+export { default as BlueSky } from "./BlueSky.astro";
+export { default as Twitter } from "./Twitter.astro";
